a51ff207c649df81d03147f0666b1d8aee69d1d7
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-13 16:56:10 +1000

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace skyss0fly\ChatHijacker;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+public function onLoad(): void {
+$this->savedefaultconfig();
+}
+
+public function onCommand(CommandSender $sender, string $bool, array $args): bool {
+$message = $thisx>getConfig()->get("Message");
+}
+
+}
